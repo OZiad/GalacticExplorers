5f0539cad4bec3061a0506897e26930a30cb10ea
Francis Tawa <Francis@tawacentral.net>
2023-08-22 19:20:36 -0400

    PUSH IT

---
+++ Galactic Explorers/Assets/Scripts/shootPosition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootPosition : MonoBehaviour
+{
+   void update(){
+        var gmp = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(gmp.x, gmp.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+   }
+}
