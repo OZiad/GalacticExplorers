a1845290f3aa4ccd505d871e3297d9764d8d5077
Francis Tawa <Francis@tawacentral.net>
2023-08-22 19:13:02 -0400

    PUSH IT

---
+++ Galactic Explorers/Assets/Scripts/bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right*speed;
+    }
+}
