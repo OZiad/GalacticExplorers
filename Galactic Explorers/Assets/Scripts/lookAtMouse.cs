3b3714ecb1007167663cceff8e923379e3c9ada3
Francis Tawa <Francis@tawacentral.net>
2023-08-22 19:25:08 -0400

    FUCK YOU

--- Galactic Explorers/Assets/Scripts/lookAtMouse.cs
+++ Galactic Explorers/Assets/Scripts/lookAtMouse.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shootPosition : MonoBehaviour
+public class lookAtMouse : MonoBehaviour
 {
-   void update(){
+    void update(){
         var gmp = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(gmp.x, gmp.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
